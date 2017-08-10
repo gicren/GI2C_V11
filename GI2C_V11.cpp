ea9224312e0e99f386d5398868e2509c25cf50ed
gicren <gicren@163.com>
2017-08-11 01:06:14 +0800

    Add files via upload

---
+++ GI2C_V11.cpp
@@ -0,0 +1,180 @@
+//*********************** Copyright Gicren Robotics and Electronics ***********************
+//*File Name    : GI2C_V11.cpp
+//*Author       : Gicren Team (www.gicren.com)
+//*Version      : V0001
+//*Data         : 11/01/2013
+//*Description  : This file provides all the GI2C_V11 firmware functions
+//*****************************************************************************************
+
+#include <Arduino.h>
+#include <Wire.h>
+#include "GI2C_V11.h"
+
+//------------------------------------------------------------
+//Constructor
+//------------------------------------------------------------
+GI2CV11::GI2CV11(unsigned char *BufAddr,unsigned char Num)
+{
+  Wire.begin(); 
+  this->BufStartAddr=BufAddr;
+  this->BufSize=Num;
+}
+
+//------------------------------------------------------------
+//Generate PID
+//------------------------------------------------------------
+unsigned char GI2CV11::GeneratePID(unsigned char PID)
+{
+  unsigned char count=0,temp=PID;
+  while(temp)
+  {
+    temp&=(temp-1);
+    count++;
+  }
+  if(count%2)
+    return (PID<<1);
+  else
+    return ((PID<<1)+1);
+}
+
+//------------------------------------------------------------
+//Check PID
+//------------------------------------------------------------
+unsigned char GI2CV11::CheckPID(unsigned char PID)
+{
+  unsigned char count=0,temp=PID;
+  while(temp)
+  {
+    temp&=(temp-1);
+    count++;
+  }
+  if(count%2)
+    return PID;
+  else 
+    return 0xff;
+}
+
+//--------------------- Write Registers ----------------------
+//SSA     : System Slave Address
+//RegAddr : The start address of user registers to be written
+//Num     : The number of bytes to be written
+//------------------------------------------------------------
+unsigned char GI2CV11::Write(unsigned char SSA,unsigned char RegAddr,unsigned char Num)
+{
+  unsigned char i,temp,check=0;
+  
+  if(((unsigned int)RegAddr+(unsigned int)Num)>this->BufSize)  
+    return 0xff;
+    
+  //Write:RPID+Data+Check
+  Wire.beginTransmission(SSA);
+  Wire.write(this->GeneratePID(RegAddr));
+  for(i=RegAddr;i<(RegAddr+Num);i++)
+  {
+    temp=*(this->BufStartAddr+(i%this->BufSize));
+    Wire.write(temp);
+    check+=temp;
+  }
+  check=~check;
+  Wire.write(check);
+  Wire.endTransmission();
+  delayMicroseconds(100);
+  
+  //Write:HPID   
+  Wire.beginTransmission(SSA);
+  Wire.write(this->GeneratePID(GI2C_HPID));
+  Wire.endTransmission();
+  delayMicroseconds(100);
+  
+  //Read:Check
+  while(Wire.available())
+    Wire.read();
+  Wire.requestFrom((int)SSA,1,true);
+  if(Wire.read()==check)
+    return 0;
+  else
+    return 0xff;
+}
+
+//---------------------- Read Registers ----------------------
+//SSA     : System Slave Address
+//RegAddr : The start address of user registers to be read
+//Num     : The number of bytes to be read
+//------------------------------------------------------------
+unsigned char GI2CV11::Read(unsigned char SSA,unsigned char RegAddr,unsigned char Num)
+{
+  unsigned char i,temp,check=0;
+  
+  if(((unsigned int)RegAddr+(unsigned int)Num)>this->BufSize)  
+    return 0xff;
+  
+  //Write:RPID
+  Wire.beginTransmission(SSA);
+  Wire.write(this->GeneratePID(RegAddr));
+  Wire.endTransmission();
+  delayMicroseconds(100);
+  
+  //Read:Data
+  while(Wire.available())
+    Wire.read();
+  Wire.requestFrom((int)SSA,(int)Num,true);
+  for(i=RegAddr;i<(RegAddr+Num);i++)
+  { 
+    temp=Wire.read();
+    *(this->BufStartAddr+(i%this->BufSize))=temp;
+    check+=temp;
+  }
+  check=~check;
+  delayMicroseconds(100);
+  
+  //Write:HPID
+  Wire.beginTransmission(SSA);
+  Wire.write(this->GeneratePID(GI2C_HPID));
+  Wire.endTransmission();
+  delayMicroseconds(100);
+  
+  //Read:Check
+  while(Wire.available())
+    Wire.read();
+  Wire.requestFrom((int)SSA,1,true);
+  if(check==Wire.read())
+    return 0;
+  else
+    return 0xff;
+}
+
+//------------------------ Send SPID -------------------------
+//SSA     : System Slave Address
+//PID     : SPID Value
+//------------------------------------------------------------
+void GI2CV11::SPID_Send(unsigned char SSA,unsigned char PID)
+{
+  Wire.beginTransmission(SSA);
+  Wire.write(this->GeneratePID(PID));
+  Wire.endTransmission();
+}
+
+//---------------------- Get Error Word ----------------------
+//SSA     : System Slave Address
+//------------------------------------------------------------
+unsigned int GI2CV11::GetError(unsigned char SSA)
+{
+  unsigned int temp;
+  //Write:EPID
+  Wire.beginTransmission(SSA);
+  Wire.write(this->GeneratePID(GI2C_EPID));
+  Wire.endTransmission();
+  delayMicroseconds(100);
+  
+  //Read:Error Word
+  while(Wire.available())
+    Wire.read();
+  Wire.requestFrom((int)SSA,2,true);
+  temp=Wire.read();
+  temp<<=8;
+  temp+=Wire.read();
+  
+  return temp;
+}
+
+//*********************** Copyright Gicren Robotics and Electronics ***********************
---
+++ GI2C_V11.h
@@ -0,0 +1,40 @@
+//*********************** Copyright Gicren Robotics and Electronics ***********************
+//* File Name    : GI2C_V11.h
+//* Author       : Gicren Team (www.gicren.com)
+//* Version      : V0001
+//* Data         : 11/01/2013
+//* Description  : This file contains all the functions prototypes for the GI2C_V11 library
+//*****************************************************************************************
+
+#ifndef _GI2C_V11_H
+#define _GI2C_V11_H
+
+#define GI2C_HPID     0x7f  //Handshake , Get the check byte of last instruction from slave device
+#define GI2C_EPID     0x7e  //Get the Error Word
+#define GI2C_SPID_FC  0x7d  //Factory settings
+#define GI2C_SPID_EE  0x7c  //Update the bytes stored in EEPROM
+#define GI2C_SPID_RS  0x7b  //Restart
+#define GI2C_SPID_EC  0x7a  //Clear the Error Word
+#define GI2C_SPID_CE  0x79  //Enable the immediate write mode
+#define GI2C_SPID_AE  0x78  //Enable the non immediate write mode
+#define GI2C_SPID_AC  0x77  //Action with data from non immediate write mode
+
+class GI2CV11
+{
+  private:
+    unsigned char *BufStartAddr;
+    unsigned char BufSize;
+    unsigned char GeneratePID(unsigned char PID);
+    unsigned char CheckPID(unsigned char PID);
+    
+  public:
+    GI2CV11(unsigned char *BufAddr,unsigned char Num);
+    unsigned char Write(unsigned char SSA,unsigned char RegAddr,unsigned char Num);
+    unsigned char Read(unsigned char SSA,unsigned char RegAddr,unsigned char Num);
+    void SPID_Send(unsigned char SSA,unsigned char PID);
+    unsigned int GetError(unsigned char SSA);
+};
+
+#endif
+
+//*********************** Copyright Gicren Robotics and Electronics ***********************
